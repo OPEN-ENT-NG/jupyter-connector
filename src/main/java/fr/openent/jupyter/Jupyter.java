eb6f33c1993ae91e87bd90feb29f5eb9ac336354
Quentin Périé <quentin.perie@cgi.com>
2021-07-09 11:07:43 +0200

    core() : First commit

---
+++ src/main/java/fr/openent/jupyter/Jupyter.java
@@ -0,0 +1,12 @@
+package fr.openent.jupyter;
+
+import org.entcore.common.http.BaseServer;
+
+public class Jupyter extends BaseServer {
+
+	@Override
+	public void start() throws Exception {
+		super.start();
+	}
+
+}
